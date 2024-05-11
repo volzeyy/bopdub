e3ade65cd5a196b6ceba0513986a45b3b60c7909
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-05-11 23:13:32 +0300

    Changed analytics place

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <Analytics/>
+      <body className={inter.className}>
+        {children}
+        <Analytics/>
+      </body>
     </html>
   )
 }
